55d34137128c768a2a63fd605fdbb57574d35241
Vivek Maskara <maskaravivek@gmail.com>
2021-05-15 01:58:21 -0700

    Emrit division

--- menu-bar-app/scripts/index.js
+++ menu-bar-app/scripts/index.js
@@ -1,7 +1,11 @@
+const EMRIT_RATIO = 0.2
+
 window.addEventListener('DOMContentLoaded', (event) => {
     document.getElementById('save_btn').addEventListener("click", function () {
         let hotspot_id_input_val = document.getElementById('hotspot_id').value
+        let is_emrit = document.getElementById("is_emrit").checked
         localStorage.setItem('hotspot_id', hotspot_id_input_val)
+        localStorage.setItem('is_emrit', is_emrit)
         showEarningsDiv()
         displayEarnings(hotspot_id_input_val)
     });
@@ -70,16 +74,22 @@ function displayEarnings(hotspot_id) {
     fetch(`https://helium-monitor.herokuapp.com/api/v1/earnings?hotspot_id=${hotspot_id}`)
         .then(response => response.json())
         .then(data => {
+            let is_emrit = localStorage.getItem('is_emrit') == "true"
+
             let latest = document.getElementById('latest-window')
-            latest.innerHTML = data['latest_window']
+            latest.innerHTML = is_emrit ? (data['latest_window'] * EMRIT_RATIO).toFixed(2) : data['latest_window']
 
             let last_day = document.getElementById('last-day-window')
-            last_day.innerHTML = data['last_day']
+            last_day.innerHTML = is_emrit ? (data['last_day'] * EMRIT_RATIO).toFixed(2) : data['last_day']
 
             let summary = document.getElementById('summary-window')
-            summary.innerHTML = data['summary_window']
+            summary.innerHTML = is_emrit ? (data['summary_window'] * EMRIT_RATIO).toFixed(2) : data['summary_window']
 
-            let earnings = data['summary_window'] * data['price']
+            let earnings = (data['summary_window'] * data['price'])
+
+            if(is_emrit) {
+                earnings *= EMRIT_RATIO
+            }
 
             let total_earnings = document.getElementById('total_earnings')
             total_earnings.innerHTML = `(${earnings.toFixed(2)} $)`
