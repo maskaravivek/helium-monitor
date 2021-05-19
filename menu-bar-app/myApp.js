3e8ffb820a5f7b79af83b9550ddcbd17adb5f190
Vivek Maskara <maskaravivek@gmail.com>
2021-05-18 23:58:41 -0700

    Check and apply updates

--- menu-bar-app/myApp.js
+++ menu-bar-app/myApp.js
@@ -4,10 +4,32 @@ const { app, ipcMain } = require('electron')
 
 const { app, autoUpdater } = require('electron')
 
+const UPDATE_CHECK_INTERVAL = 10 * 60 * 1000
+
 const server = "https://hazel-ten-rho.vercel.app"
 const feed = `${server}/update/${process.platform}/${app.getVersion()}`
 
 autoUpdater.setFeedURL(feed)
+
+const UPDATE_CHECK_INTERVAL = 10 * 60 * 1000
+
+setInterval(() => {
+  autoUpdater.checkForUpdates()
+}, UPDATE_CHECK_INTERVAL)
+
+autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+  const dialogOpts = {
+    type: 'info',
+    buttons: ['Restart', 'Later'],
+    title: 'Application Update',
+    message: process.platform === 'win32' ? releaseNotes : releaseName,
+    detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+  }
+
+  dialog.showMessageBox(dialogOpts).then((returnValue) => {
+    if (returnValue.response === 0) autoUpdater.quitAndInstall()
+  })
+})
 
 const mb = menubar({
   showDockIcon: false,
