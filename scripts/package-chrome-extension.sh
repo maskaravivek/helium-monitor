288eaa6fb17d114f7c49cc16468ad98eae94cd49
Vivek Maskara <maskaravivek@gmail.com>
2021-06-12 14:41:19 -0700

    Add script for packing extension

---
+++ scripts/package-chrome-extension.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd ..
+mkdir build
+mkdir build/helium-monitor
+cp -R menu-bar-app/ build/helium-monitor/
+cd build/helium-monitor/
+rm -rf node_modules build dist myAApp.js preload.js privacy-policy.html README.md yarn.lock package.json package-lock.json .gitignore
+cd ..
+zip -r helium-monitor.zip helium-monitor/
+
+# rm -rf helium-monitor
