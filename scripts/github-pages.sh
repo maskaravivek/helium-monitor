8a51d71b276188565b75373f066063fa1c0fe541
Vivek Maskara <maskaravivek@gmail.com>
2021-06-13 16:05:37 -0700

    Update dist

--- scripts/github-pages.sh
+++ scripts/github-pages.sh
@@ -6,3 +6,6 @@ cp -R menu-bar-app/ dist
 cd dist
 rm -rf node_modules build dist myAApp.js preload.js README.md yarn.lock package.json package-lock.json .gitignore
 cd ..
+git add .
+git commit -m "Update dist"
+git subtree push --prefix dist  origin gh-pages
