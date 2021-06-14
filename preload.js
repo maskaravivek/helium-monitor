3e645177e24268c967dbd25c4ed2e06408cb77f7
Rahul Varma <rahulsvarm@gmail.com>
2021-06-13 20:11:19 -0700

    publish: Fixed map issues
    generated from commit 180d338fbe5a512c2ef3ee0550382a27310f24c6

---
+++ preload.js
@@ -0,0 +1,22 @@
+const electron = require('electron')
+const ipc = electron.ipcRenderer
+
+window.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('coffee_btn').addEventListener("click", function () {
+        const reply = ipc.sendSync('bmc-event', 'Open BMC link in browser')
+    });
+
+    document.getElementById('new_hotspot_link').addEventListener("click", function () {
+        const reply = ipc.sendSync('new-hotspot-event', 'New hotspot')
+    });
+
+    document.getElementById('web_btn').addEventListener("click", function () {
+        let active_hotspot_id = localStorage.getItem('active_hotspot_id')
+        const reply = ipc.sendSync('hotspot-event', active_hotspot_id)
+    });
+
+    document.getElementById('emrit_hotspot_link').addEventListener("click", function () {
+        const reply = ipc.sendSync('emrit-signup-event', 'New Emrit Hotspot')
+    });
+})
+
