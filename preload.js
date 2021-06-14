129dd85ba2cecafabc11e39ae634b83669e4b2db
Raul9595 <rahulsvarm@gmail.com>
2021-06-14 04:12:54 +0000

    publish: Minor changes
    generated from commit 9c712cb84df0503938c5d6d7ed95e6041587f3dc

--- preload.js
+++ preload.js
@@ -2,21 +2,34 @@ const electron = require('electron')
 const ipc = electron.ipcRenderer
 
 window.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('coffee_btn').addEventListener("click", function () {
-        const reply = ipc.sendSync('bmc-event', 'Open BMC link in browser')
-    });
+    coffee_btn = document.getElementById('coffee_btn');
+    new_hotspot_link = document.getElementById('new_hotspot_link');
+    web_btn = document.getElementById('web_btn');
+    emrit_hotspot_link = document.getElementById('emrit_hotspot_link');
 
-    document.getElementById('new_hotspot_link').addEventListener("click", function () {
-        const reply = ipc.sendSync('new-hotspot-event', 'New hotspot')
-    });
+    if (coffee_btn) {   
+        coffee_btn.addEventListener("click", function () {
+            const reply = ipc.sendSync('bmc-event', 'Open BMC link in browser')
+        });
+    }
 
-    document.getElementById('web_btn').addEventListener("click", function () {
-        let active_hotspot_id = localStorage.getItem('active_hotspot_id')
-        const reply = ipc.sendSync('hotspot-event', active_hotspot_id)
-    });
+    if (new_hotspot_link) { 
+        new_hotspot_link.addEventListener("click", function () {
+            const reply = ipc.sendSync('new-hotspot-event', 'New hotspot')
+        });
+    }
 
-    document.getElementById('emrit_hotspot_link').addEventListener("click", function () {
-        const reply = ipc.sendSync('emrit-signup-event', 'New Emrit Hotspot')
-    });
+    if (web_btn) { 
+        web_btn.addEventListener("click", function () {
+            let active_hotspot_id = localStorage.getItem('active_hotspot_id')
+            const reply = ipc.sendSync('hotspot-event', active_hotspot_id)
+        });
+    }
+
+    if (emrit_hotspot_link) { 
+        emrit_hotspot_link.addEventListener("click", function () {
+            const reply = ipc.sendSync('emrit-signup-event', 'New Emrit Hotspot')
+        });
+    }
 })
 
